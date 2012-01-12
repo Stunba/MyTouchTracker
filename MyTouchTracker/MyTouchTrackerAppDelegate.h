a50eef7ce3ee61a94574e48ba9f5ad9169c7131d
Artjom Bastun <artem.bastun@gmail.com>
2012-01-12 22:38:58 +0300

    Initial Commit

---
+++ MyTouchTracker/MyTouchTrackerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MyTouchTrackerAppDelegate.h
+//  MyTouchTracker
+//
+//  Created by Artem Bastun on 12.01.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTouchTrackerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
